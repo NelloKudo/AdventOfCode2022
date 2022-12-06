dda280b52eb8351cbe344b1b424dd33e942b7ce1
NelloKudo <marshnelloosu@gmail.com>
2022-12-06 23:30:02 +0100

    Day 3

--- day03/es03p2.py
+++ day03/es03p2.py
@@ -1,14 +1,23 @@
 with open("day03/file.txt") as file:
     alphabet="0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     itemsum=0
+    line_counter=0
+    rucksack_list=[]
 
     for rucksack in file.readlines():
-        print(rucksack[:(len(rucksack)//2)])
-        print(rucksack[(len(rucksack)//2):])
-        for letter in rucksack[:len(rucksack)//2]:
-            if letter in rucksack[len(rucksack)//2:]:
-                print(letter)
-                itemsum+=alphabet.index(letter)
+        line_counter+=1
+        if line_counter != 4:
+            rucksack_list.append(rucksack.strip('\n'))
+        else:
+            print (rucksack_list)
+            for rucksack in rucksack_list:
+                for letter in rucksack:
+                    if letter in rucksack_list[1] and letter in rucksack_list[2]:
+                        print(letter)
+                        itemsum+=alphabet.index(letter)
+                        break
                 break
+            rucksack_list=[]
+            line_counter=0
 
     print(itemsum)
