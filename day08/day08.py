7835b8e7aee960cfe625ec02be5e89ec2af725cd
NelloKudo <marshnelloosu@gmail.com>
2022-12-13 03:18:06 +0100

    Completed Day 08

--- day08/day08.py
+++ day08/day08.py
@@ -1,5 +1,6 @@
 # For this time, I'll refer to the grid as "forest", because well. That is a forest. Right?
-# PS: I wish I knew what "all" did earlier.. lol
+# PS: My solution was pretty much the same as this video (but using reversed ranges lmao), so I adjusted some
+# stuff to it as it looked much better. Props to hyper-neutrino! https://www.youtube.com/watch?v=2lypi76wRsM
 
 def part1():
     with open ("day08/file.txt") as file:
@@ -22,20 +23,42 @@ def part1():
 print(part1())
 
 def part2():
+
     with open ("day08/file.txt") as file:
         input_file=file.read().strip().split()
         forest_grid = [list(map(int, list(line))) for line in input_file]
 
         forest_lenght=len(forest_grid[0])
         forest_height=len(forest_grid)
-        tree_counter=0
-        print(forest_grid)  
-        tree_list=[]
+        tree_counter=0 
         
         for i in range(forest_height):
-            templist=[]
             for j in range(forest_lenght):
                 tree_height=forest_grid[i][j]
-        pass
+                left = right = up = down = 0
+                
+                for n in range(j-1, -1, -1):
+                    left+=1
+                    if forest_grid[i][n] >= tree_height:
+                        break
+
+                for n in range(j+1, forest_lenght):
+                    right+=1
+                    if forest_grid[i][n] >= tree_height:
+                        break
+
+                for n in range(i-1, -1, -1):
+                    down+=1
+                    if forest_grid[n][j] >= tree_height:
+                        break
+
+                for n in range(i+1, forest_height):
+                    up+=1
+                    if forest_grid[n][j] >= tree_height:
+                        break
+
+                tree_counter=max(tree_counter, up*down*left*right)
+                
+        return tree_counter
 
 print(part2())
